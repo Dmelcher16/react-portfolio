d4db7d068e85029a85c07909d3d17e230851bbcc
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-08 11:34:27 -0700

    Updates

---
+++ precache-manifest.8a3fb2ac166249e8ed9d79c0a42e6ae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126a46a7d5a777cac7fce73c53a33f26",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b96c10bc9dc55d5b3711",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dfe7163e1162a3b89e90",
+    "url": "/react-portfolio/static/css/main.1512b25b.chunk.css"
+  },
+  {
+    "revision": "b96c10bc9dc55d5b3711",
+    "url": "/react-portfolio/static/js/2.3d65934d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.3d65934d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe7163e1162a3b89e90",
+    "url": "/react-portfolio/static/js/main.1b2f76b3.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  }
+]);
