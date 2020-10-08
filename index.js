3bb91e49467e6847d551de49c3331a5c05ad43ac
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-08 11:33:13 -0700

    fixing deployment

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from './src/index';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<Index />, document.getElementById('root'));
