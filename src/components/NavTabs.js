dc3f02aec8b7fe9a73f1c2dd16a4e032872c1eb9
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-07 23:41:22 -0700

    added styling and links

---
+++ src/components/NavTabs.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./style.css";
+
+function NavTabs() {
+  // We'll go into the Hooks API later, for now, we are just using some code
+  // from the react-router docs (https://reacttraining.com/react-router/web/api/Hooks/uselocation)
+  // This allows the component to check the route any time the user uses a link to navigate.
+  const location = useLocation();
+
+  return (
+    <ul className="nav nav-tabs bg-secondary">
+      <li className="nav-item">
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home/About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Portfolio"
+          className={location.pathname === "/Portfolio" ? "nav-link active" : "nav-link"}
+        >
+          Portfolio
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Contact"
+          className={location.pathname === "/Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact/Resume
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./styles/home_style.css";
+
+
+function Contact(props) {
+  return (
+    <div>
+      <section className="" alt="this contains and pushes the main content in/our when you resize">
+        <section className="jumbotron text-left myAdj" id="addDarkGrey">
+          <div className="container border rounded border border-dark bg-white"
+            alt="Main Container that holds the contact form">
+            <div className="container mt-2">
+              <div className="row">
+                <h1 className="ml-0 mt-4 text-success">How to contact Joe Farrish</h1>
+              </div>
+              <div className="div">
+                <hr />
+              </div>
+            </div>
+            <div className="col-dm-12 mb-4">
+              <section alt="Email Joseph Farrish.">  
+                <h4><h4>Email:<a className="col-2 text-primary" href="mailto:joseph_a_f@hotmail.com">joseph_a_f@hotmail.com</a></h4></h4> 
+              </section>
+            </div>
+            <div className="col-dm-12 mb-4">
+              <section alt="Download Joe Farrish's Resume.">
+                <h4><a className="text-danger" href="https://drive.google.com/file/d/1GKverFkQ2kJCvslThb2zZZ9K2_0wmKl7/view?usp=sharing">Resume</a></h4>
+              </section>
+            </div>
+            <div className="col-dm-12 mb-4">
+              <section alt="LinkedIn">
+                <h4><a className="text-info" href="https://www.linkedin.com/in/joseph-farrish-17b03216/">LinkedIn</a></h4>
+              </section>
+            </div>
+            <div className="col-dm-12 mb-4">
+              <section alt="GitHub">
+                <h4><a className="text-secondary" href="https://github.com/joejoe909">GitHub</a></h4>
+              </section>
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default Contact;
