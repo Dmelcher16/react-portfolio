bfa347d2922fded65578b876b80854183fa38077
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-08 11:45:30 -0700

    moving folders for deployment

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import Portfolio from "./components/pages/Portfolio";
-import Contact from "./components/pages/Contact";
+import NavTabs from "./src/components/NavTabs";
+import Home from "./src/components/pages/Home";
+import Portfolio from "./src/components/pages/Portfolio";
+import Contact from "./src/components/pages/Contact";
 import "./components/style.css";
-import Footer from "./components/Footer/index.js";
+import Footer from "./src/components/Footer/index.js";
 
 
 function App() {
